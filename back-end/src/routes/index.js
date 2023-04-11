31d3d383eb061e972dfb09b0a432a3efe5511d37
alexis <kkalexander717@gmail.com>
2023-04-11 20:30:53 +0300

    login route

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,5 +1,8 @@
 import { testRoute } from './testRoute';
-
+import { signUpRoute } from './signUpRoute';
+import { loginRoute } from './loginRoute';
 export const routes = [
+    signUpRoute,
     testRoute,
+    loginRoute
 ];
